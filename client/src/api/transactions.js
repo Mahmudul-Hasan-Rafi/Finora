0a5703296c7156679d92a8a51ae1a4c73b57fa76
Mahmudul-Hasan-Rafi <mhrafi551@gmail.com>
2026-08-18 13:14:34 +0600

    Add transactions API helper functions

---
+++ client/src/api/transactions.js
@@ -0,0 +1,16 @@
+import api from "./axios";
+
+export const getTransactions = async () => {
+  const res = await api.get("/transactions");
+  return res.data;
+};
+
+export const createTransaction = async (data) => {
+  const res = await api.post("/transactions", data);
+  return res.data;
+};
+
+export const deleteTransaction = async (id) => {
+  const res = await api.delete(`/transactions/${id}`);
+  return res.data;
+};
